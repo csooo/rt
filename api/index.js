aa413fd40d4612a3a581f44b0d94b85020b54d71
csooo <87272311+csooo@users.noreply.github.com>
2023-11-23 19:46:45 +0100

    Update index.js

--- api/index.js
+++ api/index.js
@@ -1,51 +1,62 @@
-const express = require('express');
+import express from "express";
+import cors from "cors";
 const app = express();
-const cors = require('cors')
-app.use(cors({
-        origin: 'https://rt-x4vb-csooo.vercel.app/'
-}))
+
+app.use(
+  cors({
+    origin: "https://rt-x4vb-csooo.vercel.app/",
+  })
+);
 const port = 5000;
 app.use(express.json());
-const mongoose = require('mongoose');
-mongoose.connect('mongodb+srv://password:SHpq6uDbAKJNNxB2@cluster0.yxevysz.mongodb.net/?retryWrites=true&w=majority');
+import mongoose from "mongoose";
+mongoose.connect(
+  "mongodb+srv://password:SHpq6uDbAKJNNxB2@cluster0.yxevysz.mongodb.net/?retryWrites=true&w=majority"
+);
 
-const User = mongoose.model('User', new mongoose.Schema({
+const User = mongoose.model(
+  "User",
+  new mongoose.Schema({
     count: {
-        type: Number,
+      type: Number,
     },
     content: {
-        type: String,
+      type: String,
     },
     date: {
-        type: Date,
-        default: Date.now,
+      type: Date,
+      default: Date.now,
     },
-}));
-app.get('/'), async (req, res) => {
-        res.status(201).send({ message: 'User added successfully'});
-    }
-app.post('/', async (req, res) => {
-    const {count,content} = req.body
-    console.log(count,content)
-    try {
-        // Create a new user with the provided data
-        const newUser = new User({
-            count: count,
-            content: content,
-        });
+  })
+);
+app.get("/"),
+  async (req, res) => {
+    res.status(201).send({ message: "User added successfully" });
+  };
+app.post("/", async (req, res) => {
+  const { count, content } = req.body;
+  console.log(count, content);
+  try {
+    // Create a new user with the provided data
+    const newUser = new User({
+      count: count,
+      content: content,
+    });
 
-        // Save the user to the database
-        const savedUser = await newUser.save();
+    // Save the user to the database
+    const savedUser = await newUser.save();
 
-        // Log the saved user details
-        console.log('User saved:', savedUser);
+    // Log the saved user details
+    console.log("User saved:", savedUser);
 
-        // Send a response
-        res.status(201).send({ message: 'User added successfully', user: savedUser });
-    } catch (error) {
-        console.error('Error adding user:', error);
-        res.status(500).send({ message: 'Internal Server Error' });
-    }
+    // Send a response
+    res
+      .status(201)
+      .send({ message: "User added successfully", user: savedUser });
+  } catch (error) {
+    console.error("Error adding user:", error);
+    res.status(500).send({ message: "Internal Server Error" });
+  }
 });
 
-export default app
+export default app;
